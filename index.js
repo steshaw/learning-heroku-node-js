d1adc970c5f0f7e73392c1476bfddfd594aa48ff
Steven Shaw <steven@steshaw.org>
2019-05-22 14:39:19 +1000

    In need of type-checker

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const Pool = require('pg')
+const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
