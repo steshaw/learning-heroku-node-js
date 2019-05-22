9ff00d6327f836414a6b3ea3c427f10c58eb58e3
Steven Shaw <steven@steshaw.org>
2019-05-22 14:24:43 +1000

    ✨ Implement /db

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const POOL = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
 showTimes = () => {
   let result = ''
@@ -17,6 +22,18 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('select * from test_table')
+      const results = { 'results': (result) ? result.rows : null }
+      res.render('pages/db', results)
+      client.release()
+    } catch (err) {
+      console.error(err)
+      res.send("Error: " + err)
+    }
+  })
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
