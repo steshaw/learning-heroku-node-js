7696a6730739c75b430265c08ece4e0b5a96df19
Steven Shaw <steven@steshaw.org>
2019-05-22 15:42:18 +1000

    Default to port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
